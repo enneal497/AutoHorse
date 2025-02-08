d26b04e6293a325525f0dc5b665876f793891f4b
enneal497 <enneal497@gmail.com>
2025-02-08 17:42:46 +0000

    Update MarkerHandler.cpp

--- src/MarkerHandler.cpp
+++ src/MarkerHandler.cpp
@@ -6,8 +6,8 @@ namespace AutoHorse {
     bool MarkerHandler::GetMarker()
     {
         auto player = RE::PlayerCharacter::GetSingleton();
-        RE::ObjectRefHandle markerRef = REL::Module::IsVR() ? player->GetVRInfoRuntimeData()->playerMapMarker : player->GetInfoRuntimeData().playerMapMarker;
 
+        RE::ObjectRefHandle markerRef = REL::Module::IsVR() ? player->GetVRInfoRuntimeData()->playerMapMarker : player->GetInfoRuntimeData().playerMapMarker;
 
         if (!markerRef) {
             GetQuestMarker();
@@ -25,18 +25,13 @@ namespace AutoHorse {
     void MarkerHandler::GetPlayerMarker(RE::ObjectRefHandle markerRef)
     {
         auto marker = markerRef.get().get();
+        if (!marker) { return; }
+        //auto m = marker->data.location;
+        auto markerPos = marker->GetPosition();
 
-        markerPosition = marker->GetPosition();
+        logger::info("x: {}", markerPos.x);
 
-        float xPos = marker->GetPositionX();
-        float yPos = marker->GetPositionY();
-        float zPos = marker->GetPositionZ();
-
-        logger::info("xPos: {}", xPos);
-        logger::info("yPos: {}", yPos);
-        logger::info("zPos: {}", zPos);
-
-        //MoveTargetMarker();
+        MoveTargetMarker(markerPos);
     }
 
     //Get closest quest marker if available
@@ -45,10 +40,37 @@ namespace AutoHorse {
         
     }
 
-    void MarkerHandler::MoveTargetMarker()
+    void MarkerHandler::MoveTargetMarker(RE::NiPoint3 markerPos)
     {
         //Replace with actual quest ID
+        /*
         RE::TESQuest* controlQuest = RE::TESDataHandler::GetSingleton()->LookupForm<RE::TESQuest>(Settings::questID, Settings::espName);
+        auto aliases = controlQuest->aliases;
+        if (aliases.size() < 2) {
+            return;
+        }
+        auto refAlias = static_cast<RE::BGSRefAlias*>(aliases[2]);
+        logger::info("RefAlias name: {}", refAlias->aliasName.c_str());
+        if (refAlias) {
+            logger::info("refAlias obtained");
+            auto ref = refAlias->GetReference();
+            if (ref) {
+                ref->data.location = markerPos;
+            }
+            else {
+                logger::info("No ref found");
+            }
+        }
+        */
+        auto ref = RE::TESDataHandler::GetSingleton()->LookupForm(Settings::markerID, Settings::espName)->As<RE::TESObjectREFR>();
+        if (!ref) {
+            logger::info("No ref found");
+        }
+        else {
+            logger::info("Ref found");
+            ref->data.location = markerPos;
+            ref->Update3DPosition(true);
+        }
 
         //auto alias = targetQuest->aliases[0];
 
