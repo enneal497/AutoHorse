968fe95432d9ef2b25511636e3453494758e992d
enneal497 <enneal497@gmail.com>
2025-02-08 13:26:12 +0000

    Update MarkerHandler.h

--- include/MarkerHandler.h
+++ include/MarkerHandler.h
@@ -5,14 +5,11 @@ namespace AutoHorse {
     class MarkerHandler
     {
     public:
-        static void GetMarker();
+        static bool GetMarker();
         static void GetPlayerMarker(RE::ObjectRefHandle markerRef);
         static void GetQuestMarker();
+        static void MoveTargetMarker();
     private:
-        static inline float xPos;
-        static inline float yPos;
-        static inline float zPos;
-
         static inline RE::NiPoint3 markerPosition;
         //static void GetQuestAlias(RE::TESQuest* ownerQuest, uint8_t aliasNum);
         //static bool TestRefLocation();
