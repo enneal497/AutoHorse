3192ed8ed190d2e374f2ac75540d40277ede7319
enneal497 <enneal497@gmail.com>
2025-02-08 13:27:31 +0000

    Update InputEventHandler.cpp

--- src/InputEventHandler.cpp
+++ src/InputEventHandler.cpp
@@ -15,12 +15,50 @@ namespace AutoHorse {
         if (inputDeviceManager) {
             inputDeviceManager->AddEventSink(GetSingleton());
             logger::info("Event sink initialized.");
+
+            isReady = RE::TESDataHandler::GetSingleton()->LookupForm<RE::TESGlobal>(Settings::globalID, Settings::espName);
+            controlQuest = RE::TESDataHandler::GetSingleton()->LookupForm<RE::TESQuest>(Settings::questID, Settings::espName);
+
         }
         else {
             stl::report_and_fail("Failed to initialize event sink.");
         }
     }
 
+    //NOT CURRENTLY IN USE
+    /*
+    bool InputEventHandler::SetStage(RE::TESQuest* a_quest, std::uint16_t a_stage)
+    {
+        using func_t = decltype(&SetStage);
+        REL::Relocation<func_t> func{ RELOCATION_ID(24482, 25004) };
+        return func(a_quest, a_stage);
+    }
+    */
+
+    void InputEventHandler::StartAutopilot() {
+        bool markerReady = MarkerHandler::GetMarker();
+        if (markerReady) {
+            controlQuest->Start();
+            //SetStage(controlQuest, 0);
+        }
+
+    }
+
+    void InputEventHandler::StopAutopilot(bool dismount, RE::ActorPtr mount) {
+        if (controlQuest->IsRunning()) {
+            controlQuest->Stop();
+            if (dismount) {
+                //Dismount horse
+                const auto player = RE::PlayerCharacter::GetSingleton();
+                if (!player || !player->Is3DLoaded()) {
+                    return;
+                }
+
+                mount->ActivateRef(player, 0, mount->GetBaseObject(), 1, false);
+            }
+        }
+    }
+    
     RE::BSEventNotifyControl InputEventHandler::ProcessEvent(RE::InputEvent* const* a_event, RE::BSTEventSource<RE::InputEvent*>* a_eventSource) {
 
         UNREFERENCED_PARAMETER(a_eventSource);
@@ -30,6 +68,12 @@ namespace AutoHorse {
         }
         const auto player = RE::PlayerCharacter::GetSingleton();
         if (!player || !player->Is3DLoaded()) {
+            return RE::BSEventNotifyControl::kContinue;
+        }
+
+        RE::NiPointer<RE::Actor> mount;
+        if (!(player->GetMount(mount))) {
+            //logger::info("Not on mount");
             return RE::BSEventNotifyControl::kContinue;
         }
 
@@ -57,20 +101,22 @@ namespace AutoHorse {
                 if (device == RE::INPUT_DEVICE::kKeyboard) {
                     logger::info("key {}", keyname);
 
-                    if (key == Settings::ReturnControls(KeyType::Activate)) {
+                    if (key == Settings::ReturnControls(KeyType::Start)) {
                         // Start autopilot
                         logger::info("Start autopilot");
-                        MarkerHandler::GetMarker();
+                        StartAutopilot();
 
                     }
-                    else if (key == Settings::ReturnControls(KeyType::Dismount)) {
+                    else if (key == Settings::ReturnControls(KeyType::Activate)) {
                         // Stop autopilot and dismount
                         logger::info("Stop autopilot and dismount");
+                        StopAutopilot(true, mount);
 
                     }
-                    else if (key == Settings::ReturnControls(KeyType::Forward) || key == Settings::ReturnControls(KeyType::Back)) {
+                    else if (key == Settings::ReturnControls(KeyType::Forward)) {
                         // Stop autopilot
                         logger::info("Stop autopilot");
+                        StopAutopilot(false, mount);
 
                     }
                     else if (key == Settings::ReturnControls(KeyType::Left) || key == Settings::ReturnControls(KeyType::Right)) {
