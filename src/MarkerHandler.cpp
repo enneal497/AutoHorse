d3244275d9881441f865153fd2ae00fa0ee94ba7
enneal497 <enneal497@gmail.com>
2025-02-14 08:53:14 +0000

    Update MarkerHandler.cpp

--- src/MarkerHandler.cpp
+++ src/MarkerHandler.cpp
@@ -3,6 +3,19 @@
 
 namespace AutoHorse {
 
+    inline RE::NiPoint3 GetRealPosition(const RE::TESObjectREFR* a_objRef)
+    {
+        RE::NiPoint3 position = a_objRef->GetPosition();
+        if (const RE::TESWorldSpace* worldSpace = a_objRef->GetWorldspace())
+        {
+            RE::NiPoint3 worldSpaceOffset{ worldSpace->worldMapOffsetData.mapOffsetX,
+                                                  worldSpace->worldMapOffsetData.mapOffsetY,
+                                                  worldSpace->worldMapOffsetData.mapOffsetZ };
+            position += worldSpaceOffset * worldSpace->worldMapOffsetData.mapScale;
+        }
+        return position;
+    }
+
     bool MarkerHandler::GetMarker()
     {
         auto player = RE::PlayerCharacter::GetSingleton();
@@ -10,72 +23,108 @@ namespace AutoHorse {
         RE::ObjectRefHandle markerRef = REL::Module::IsVR() ? player->GetVRInfoRuntimeData()->playerMapMarker : player->GetInfoRuntimeData().playerMapMarker;
 
         if (!markerRef) {
-            GetQuestMarker();
+            return GetQuestMarker();
         }
         else {
-            GetPlayerMarker(markerRef);
+            return GetPlayerMarker(markerRef);
         }
-
-        //TEMP
-        return true;
 
     }
 
-    //Get custom player-set marker location
-    void MarkerHandler::GetPlayerMarker(RE::ObjectRefHandle markerRef)
+    //Get player-set marker location
+    bool MarkerHandler::GetPlayerMarker(RE::ObjectRefHandle markerRef)
     {
         auto marker = markerRef.get().get();
-        if (!marker) { return; }
-        //auto m = marker->data.location;
-        auto markerPos = marker->GetPosition();
+        if (!marker) { return false; }
+        
+        auto markerPos = GetRealPosition(marker);
+        //logger::info("x: {}", markerPos.x);
 
-        logger::info("x: {}", markerPos.x);
-
-        MoveTargetMarker(markerPos);
+        return MoveTargetMarker(markerPos);
     }
 
-    //Get closest quest marker if available
-    void MarkerHandler::GetQuestMarker()
+    //Get available quest marker
+    bool MarkerHandler::GetQuestMarker()
+    {
+        auto player = RE::PlayerCharacter::GetSingleton();
+
+        
+        RE::BSTArray<RE::BGSInstancedQuestObjective>& playerObjectives = REL::Module::IsVR() ?
+            player->GetVRPlayerRuntimeData().objectives :
+            player->GetPlayerRuntimeData().objectives;
+        
+        for (int i = playerObjectives.size() - 1; i >= 0; i--)
+        {
+            RE::BGSInstancedQuestObjective* playerObjective = &playerObjectives[i];
+            RE::BGSQuestObjective* questObjective = playerObjective->Objective;
+
+            if (!(questObjective->ownerQuest->IsActive() && questObjective->state == RE::QUEST_OBJECTIVE_STATE::kDisplayed))
+            {
+                logger::info("Quest objective not displayed");
+                continue;
+            }
+
+            for (uint32_t j = 0; j < questObjective->numTargets; j++)
+            {
+                auto questTarget = questObjective->targets[j];
+                auto aliasID = questTarget->alias;
+                auto alias = questObjective->ownerQuest->aliases[aliasID];
+
+
+                auto &name = alias->aliasName;
+                logger::info("Alias name: {}", name.c_str());
+
+                if (RE::BGSRefAlias *refAlias = static_cast<RE::BGSRefAlias*>(alias))
+                {
+                    if (auto ref = refAlias->GetReference()) {
+
+                        if (ref->GetWorldspace() == player->GetWorldspace()) {
+                            logger::info("Marker is in current worldspace");
+                            auto loc = GetRealPosition(ref);
+
+                            logger::info("Loc x: {}", loc.x);
+                            logger::info("Loc y: {}", loc.y);
+                            logger::info("Loc z: {}", loc.z);
+
+                            return MoveTargetMarker(loc);
+                        }
+                        else {
+                            logger::info("Marker is not in current worldspace");
+                        }
+                    }
+                }
+            }
+
+        }
+
+        return false;
+    }
+
+    //Move the player-set marker
+    //A custom marker is used so that the AI works even if the player removes the 'Custom Destination' marker
+    bool MarkerHandler::MoveTargetMarker(RE::NiPoint3 markerPos)
     {
         
-    }
-
-    void MarkerHandler::MoveTargetMarker(RE::NiPoint3 markerPos)
-    {
-        //Replace with actual quest ID
-        /*
-        RE::TESQuest* controlQuest = RE::TESDataHandler::GetSingleton()->LookupForm<RE::TESQuest>(Settings::questID, Settings::espName);
-        auto aliases = controlQuest->aliases;
-        if (aliases.size() < 2) {
-            return;
-        }
-        auto refAlias = static_cast<RE::BGSRefAlias*>(aliases[2]);
-        logger::info("RefAlias name: {}", refAlias->aliasName.c_str());
-        if (refAlias) {
-            logger::info("refAlias obtained");
-            auto ref = refAlias->GetReference();
-            if (ref) {
-                ref->data.location = markerPos;
-            }
-            else {
-                logger::info("No ref found");
-            }
-        }
-        */
         auto ref = RE::TESDataHandler::GetSingleton()->LookupForm(Settings::markerID, Settings::espName)->As<RE::TESObjectREFR>();
         if (!ref) {
             logger::info("No ref found");
+            return false;
         }
         else {
             logger::info("Ref found");
             ref->data.location = markerPos;
             ref->Update3DPosition(true);
+            return true;
         }
 
-        //auto alias = targetQuest->aliases[0];
-
-        //markerRef->data.location = markerPosition;
-
     }
+    /*
+    void MarkerHandler::ForceRefTo(RE::TESQuest* a_quest, uint32_t a_aliasID, RE::TESObjectREFR* a_reference)
+    {
+        using func_t = decltype(&ForceRefTo);
+        REL::Relocation<func_t> func{ RELOCATION_ID(24523, 25052) };
+        return func(a_quest, a_aliasID, a_reference);
+    }
+    */
 
 }
