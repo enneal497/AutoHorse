9a4aee750ddb86cf260c8b763c3b4195577698ef
enneal497 <enneal497@gmail.com>
2025-02-22 18:51:24 +0000

    Update Hooks.cpp

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -2,13 +2,6 @@
 
 namespace Hooks
 {
-    void Install() noexcept
-    {
-        REL::Relocation main_update{ RELOCATION_ID(35565, 36564), REL::Relocate(0x748, 0xc26, 0x7ee) };
-        stl::write_thunk_call<MainUpdate>(main_update);
-        logger::info("Installed main update hook");
-        logger::info("");
-    }
+    
 
-    i32 MainUpdate::Thunk() noexcept { return func(); }
 } // namespace Hooks
