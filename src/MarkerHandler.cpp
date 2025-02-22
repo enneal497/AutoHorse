3c02e2102d50cb1787d416e34c2874480c8bc29c
enneal497 <enneal497@gmail.com>
2025-02-22 18:52:09 +0000

    Update MarkerHandler.cpp

--- src/MarkerHandler.cpp
+++ src/MarkerHandler.cpp
@@ -22,12 +22,11 @@ namespace AutoHorse {
 
         RE::ObjectRefHandle markerRef = REL::Module::IsVR() ? player->GetVRInfoRuntimeData()->playerMapMarker : player->GetInfoRuntimeData().playerMapMarker;
 
-        if (!markerRef) {
-            return GetQuestMarker();
-        }
-        else {
+        if (markerRef) {
             return GetPlayerMarker(markerRef);
         }
+
+        return false;
 
     }
 
@@ -41,63 +40,6 @@ namespace AutoHorse {
         //logger::info("x: {}", markerPos.x);
 
         return MoveTargetMarker(markerPos);
-    }
-
-    //Get available quest marker
-    bool MarkerHandler::GetQuestMarker()
-    {
-        auto player = RE::PlayerCharacter::GetSingleton();
-
-        
-        RE::BSTArray<RE::BGSInstancedQuestObjective>& playerObjectives = REL::Module::IsVR() ?
-            player->GetVRPlayerRuntimeData().objectives :
-            player->GetPlayerRuntimeData().objectives;
-        
-        for (int i = playerObjectives.size() - 1; i >= 0; i--)
-        {
-            RE::BGSInstancedQuestObjective* playerObjective = &playerObjectives[i];
-            RE::BGSQuestObjective* questObjective = playerObjective->Objective;
-
-            if (!(questObjective->ownerQuest->IsActive() && questObjective->state == RE::QUEST_OBJECTIVE_STATE::kDisplayed))
-            {
-                logger::info("Quest objective not displayed");
-                continue;
-            }
-
-            for (uint32_t j = 0; j < questObjective->numTargets; j++)
-            {
-                auto questTarget = questObjective->targets[j];
-                auto aliasID = questTarget->alias;
-                auto alias = questObjective->ownerQuest->aliases[aliasID];
-
-
-                auto &name = alias->aliasName;
-                logger::info("Alias name: {}", name.c_str());
-
-                if (RE::BGSRefAlias *refAlias = static_cast<RE::BGSRefAlias*>(alias))
-                {
-                    if (auto ref = refAlias->GetReference()) {
-
-                        if (ref->GetWorldspace() == player->GetWorldspace()) {
-                            logger::info("Marker is in current worldspace");
-                            auto loc = GetRealPosition(ref);
-
-                            logger::info("Loc x: {}", loc.x);
-                            logger::info("Loc y: {}", loc.y);
-                            logger::info("Loc z: {}", loc.z);
-
-                            return MoveTargetMarker(loc);
-                        }
-                        else {
-                            logger::info("Marker is not in current worldspace");
-                        }
-                    }
-                }
-            }
-
-        }
-
-        return false;
     }
 
     //Move the player-set marker
@@ -118,13 +60,5 @@ namespace AutoHorse {
         }
 
     }
-    /*
-    void MarkerHandler::ForceRefTo(RE::TESQuest* a_quest, uint32_t a_aliasID, RE::TESObjectREFR* a_reference)
-    {
-        using func_t = decltype(&ForceRefTo);
-        REL::Relocation<func_t> func{ RELOCATION_ID(24523, 25052) };
-        return func(a_quest, a_aliasID, a_reference);
-    }
-    */
 
 }
